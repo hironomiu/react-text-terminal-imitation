16e86fbf2e2bf54291b11a2e23baf95d7c7d4a22
hironomiu <hironomiu@gmail.com>
2022-01-19 17:24:38 +0900

    commited

--- src/components/Response.js
+++ src/components/Response.js
@@ -6,14 +6,15 @@ commands.set('help', `へるぷ\nコマンド一覧`)
 commands.set('hello', `hello!!!\nこんにちは！！！`)
 
 const Response = ({ value }) => {
-  if (value.length === 0) return <></>
-  if (commands.get(value)) {
-    return <div className="div-hoge">{commands.get(value)}</div>
-  } else if (value.split(' ')[0] === 'echo') {
-    console.log(value.split(' '))
-    return <div className="div-hoge">{value.split(' ')[1]}</div>
+  const trimedValue = value.trimStart()
+  if (trimedValue.length === 0) return <></>
+  if (commands.get(trimedValue)) {
+    return <div className="div-hoge">{commands.get(trimedValue)}</div>
+  } else if (trimedValue.split(' ')[0] === 'echo') {
+    console.log(trimedValue.split(' '))
+    return <div className="div-hoge">{trimedValue.split(' ')[1]}</div>
   } else {
-    return <div>command not found: {value}</div>
+    return <div>command not found: {trimedValue}</div>
   }
 }
 
