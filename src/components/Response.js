b5662dd4772df7416e53220ff270de794ac3a781
hironomiu <hironomiu@gmail.com>
2022-01-19 15:31:49 +0900

    commited

--- src/components/Response.js
+++ src/components/Response.js
@@ -7,9 +7,14 @@ commands.set('hello', `hello!!!\nこんにちは！！！`)
 
 const Response = ({ value }) => {
   if (value.length === 0) return <></>
-  if (commands.get(value))
+  if (commands.get(value)) {
     return <div className="div-hoge">{commands.get(value)}</div>
-  return <div>command not found: {value}</div>
+  } else if (value.split(' ')[0] === 'echo') {
+    console.log(value.split(' '))
+    return <div className="div-hoge">{value.split(' ')[1]}</div>
+  } else {
+    return <div>command not found: {value}</div>
+  }
 }
 
 export default Response
